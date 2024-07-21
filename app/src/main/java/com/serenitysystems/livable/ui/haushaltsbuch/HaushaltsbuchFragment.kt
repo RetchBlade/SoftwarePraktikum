c0e170161d496280866a5921ac2842b6911b861c
safakkaraalp <110606414+safakkaraalp@users.noreply.github.com>
2024-07-21 20:15:55 +0200

    Haushalt Frontend - Basic Backend für Circle

--- app/src/main/java/com/serenitysystems/livable/ui/haushaltsbuch/HaushaltsbuchFragment.kt
+++ app/src/main/java/com/serenitysystems/livable/ui/haushaltsbuch/HaushaltsbuchFragment.kt
@@ -1,6 +1,5 @@
 package com.serenitysystems.livable.ui.haushaltsbuch
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,40 +7,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import com.serenitysystems.livable.R
 import com.serenitysystems.livable.databinding.FragmentHaushaltsbuchBinding
-
 
 class HaushaltsbuchFragment : Fragment() {
 
-    // Binding-Objekt, das nur zwischen onCreateView und onDestroyView gültig ist
     private var _binding: FragmentHaushaltsbuchBinding? = null
-
-    // Dieses Property ist nur zwischen onCreateView und onDestroyView gültig
     private val binding get() = _binding!!
+    private lateinit var haushaltsbuchViewModel: HaushaltsbuchViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,  // LayoutInflater zum Aufblasen von Layouts
-        container: ViewGroup?,    // Container, in den das Fragment eingefügt wird
-        savedInstanceState: Bundle? // gespeicherter Zustand
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        // Initialisieren des ViewModels
-        val wochenplanViewModel =
+        haushaltsbuchViewModel =
             ViewModelProvider(this).get(HaushaltsbuchViewModel::class.java)
 
-        // Aufblasen des Layouts für dieses Fragment
         _binding = FragmentHaushaltsbuchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // Finden der TextView und Beobachten der LiveData des ViewModels
-        val textView: TextView = binding.textHome
-        wochenplanViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it // Aktualisieren des Textes in der TextView, wenn sich die LiveData ändert
+        val textView: TextView = binding.textMonat
+        haushaltsbuchViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
-        return root // Zurückgeben der Wurzelansicht
+
+        return root
     }
 
-    // Aufräumen des Binding-Objekts, um Speicherlecks zu vermeiden
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
