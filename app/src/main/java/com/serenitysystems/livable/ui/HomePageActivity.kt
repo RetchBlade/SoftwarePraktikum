eadce4a77f0c0ddf7526d706b3f73c3fb9a26789
RetchBlade <118535671+RetchBlade@users.noreply.github.com>
2024-10-14 08:21:29 +0200

    Schreib Fehler gefixt

--- app/src/main/java/com/serenitysystems/livable/ui/HomePageActivity.kt
+++ app/src/main/java/com/serenitysystems/livable/ui/HomePageActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.PopupWindowCompat
 import com.serenitysystems.livable.R
